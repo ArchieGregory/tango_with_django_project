f6db65bfdea111941cc1d7379c24a725e81db7ff
Archie Greogry <2399335g@student.gla.ac.uk>
2020-01-24 15:30:37 +0000

    second lab commit

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    context_dict = {'boldmessage':'Crunchy, Creamy, cookie, candy, cupcake, cucumbers!'}
+    return render(request, 'rango/index.html', context= context_dict)
