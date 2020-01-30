143e3e03619093329bfb3929fc8858856a0a2eeb
Archie Greogry <2399335g@student.gla.ac.uk>
2020-01-30 19:03:28 +0000

    chapter 5

--- rango/views.py
+++ rango/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    context_dict = {'boldmessage':'Crunchy, Creamy, cookie, candy, cupcake, cucumbers!'}
+    context_dict = {'boldmessage':'Crunchy, Creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context= context_dict)
